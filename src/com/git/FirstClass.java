2aa930ff2af94c59d7a91ecac2b8d61ea8821177
huhualong <huhualong97@.16.com>
2019-06-10 14:50:07 +0800

    修改代码

--- src/com/git/FirstClass.java
+++ src/com/git/FirstClass.java
@@ -8,5 +8,11 @@ public class FirstClass {
         System.out.println("Hello git");
         System.out.println("Hello git");
         System.out.println("Hello git");
+
+
+        for (int i = 0; i < 10 ; i++) {
+
+            System.out.println("i: = " + i);
+        }
     }
 }
