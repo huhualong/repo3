975a8831e92b4c58f82eab5b063606243ce0e4dc
huhualong <huhualong97@.16.com>
2019-06-10 14:38:07 +0800

    把本地工程添加git

---
+++ src/com/git/FirstClass.java
@@ -0,0 +1,12 @@
+package com.git;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+    }
+}
